452eabcb3dfca47e5660d792a7cd033dfa9340b9
Terror4000rus <Terror4000rus@mail.ru>
2018-10-05 17:40:29 +0700

    Шоколадка Тревису.

--- test/run-test.sh
+++ test/run-test.sh
@@ -241,7 +241,7 @@ function run_byond_tests {
         source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
     fi
     run_test_ci "check globals build" "python tools/GenerateGlobalVarAccess/gen_globals.py baystation12.dme code/_helpers/global_access.dm"
-    run_test "check globals unchanged" "md5sum -c - <<< '9ba6340ee42bcf0f0ccb949026e24f8e *code/_helpers/global_access.dm'"
+    run_test "check globals unchanged" "md5sum -c - <<< 'f3d4bf8c0372aadafcc94ff120a9fc89 *code/_helpers/global_access.dm'"
     run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH baystation12.dme"
     run_test "check no warnings in build" "grep ', 0 warnings' build_log.txt"
     run_test "run unit tests" "DreamDaemon baystation12.dmb -invisible -trusted -core 2>&1 | tee log.txt"
