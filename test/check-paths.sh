a5f27f5769f059ab7c8c0acbd8a9aee721be105b
Terror4000rus <Terror4000rus@mail.ru>
2019-01-05 21:32:22 +0700

    Убираем лишние <<

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -31,7 +31,7 @@ exactly 12 "/obj text paths" '"/obj'
 exactly 8 "/turf text paths" '"/turf'
 exactly 1 "world<< uses" 'world<<|world[[:space:]]<<'
 exactly 44 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
-exactly 647 "<< uses" '(?<!<)<<(?!<)' -P
+exactly 648 "<< uses" '(?<!<)<<(?!<)' -P
 exactly 0 "incorrect indentations" '^( {4,})' -P
 exactly 28 "text2path uses" 'text2path'
 exactly 2 "update_icon() override" '/update_icon\((.*)\)'  -P
