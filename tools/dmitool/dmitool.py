3485b5a7771ab1f9f1933b8885855d1b96d1904d
Archemagus <32466328+Archemagus@users.noreply.github.com>
2019-05-26 14:01:55 +0300

    Кляти конец строки

--- tools/dmitool/dmitool.py
+++ tools/dmitool/dmitool.py
@@ -49,7 +49,7 @@ def info(filepath):
     if len(data) > 0:
         header = data.pop(0).split(",")
         # don't need to parse states, it's redundant
-        if _safe_parse(result, "images", lambda: int(header[0].split()[0].strip()))
+        if _safe_parse(result, "images", lambda: int(header[0].split()[0].strip())) != False:
 			print("Aga, popalsya! File raspolojen v: %s" % (filepath))
         _safe_parse(result, "size", lambda: header[2].split()[1].strip())
 
