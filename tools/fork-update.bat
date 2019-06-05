f551f832aa0d3467d2cadf910dd3cff4d090f0b7
AlexMorgan3817 <Ivan.Zvereff@gmail.com>
2019-06-05 14:30:37 +0700

    Утилита для обновы форка

---
+++ tools/fork-update.bat
@@ -0,0 +1,20 @@
+@echo off
+cd ../
+
+echo ///Choose branch to pull///
+set /p Data="Branch: "
+echo Branch to pull: %Data%
+
+set /p repname="Name to remote repository: "
+echo Remote repository name: %repname%
+git fetch https://github.com/infinitystation/Baystation12
+git remote add %repname% https://github.com/infinitystation/Baystation12.git
+echo Avalible now remote repositories:
+git remote
+set /p updbranch="Choose branch to update by %Data% commits from %repname%: "
+
+git checkout %updbranch%
+git pull %repname% %Data%
+echo Update completed
+set /p end_ans="Push to origin? [1(Yes)/0(No)): "
+if %end_ans%==1 git push origin master
