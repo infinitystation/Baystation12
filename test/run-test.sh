2f559835b61f608c8d02840823462a34faa9892a
AlexMorgan3817 <Ivan.Zvereff@gmail.com>
2019-06-14 17:11:43 +0700

    Фикс глобалок для Тревиса

--- test/run-test.sh
+++ test/run-test.sh
@@ -206,7 +206,7 @@ function run_code_tests {
     run_test_fail "no invalid spans" "grep -En \"<\s*span\s+class\s*=\s*('[^'>]+|[^'>]+')\s*>\" **/*.dm"
     run_test "code quality checks" "test/check-paths.sh"
     run_test "indentation check" "awk -f tools/indentation.awk **/*.dm"
-    run_test "check changelog example unchanged" "md5sum -c - <<< '683a3e0d21b90581ae6e4c95052d461e *html/changelogs/example.yml'"
+    run_test "check changelog example unchanged" "md5sum -c - <<< '80c39b95ef8e53af5dbfe03b786f6d97 *html/changelogs/example.yml'"
     run_test "check tags" "python2 tools/TagMatcher/tag-matcher.py ."
     run_test "check color hex" "python3 tools/ColorHexChecker/color-hex-checker.py ."
     run_test "check punctuation" "python2 tools/PunctuationChecker/punctuation-checker.py ."
