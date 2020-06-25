29d4868a8edf9039755509835b76bafdfe7a6d34
AlexMorgan3817 <Ivan.Zvereff@gmail.com>
2020-06-25 16:20:09 +0700

    Автоматическое востановление пустых лайнэндингов
    Изначальный скрипт atik1n

---
+++ tools/simpleutilites/lineendings.py
@@ -0,0 +1,30 @@
+import os
+from os.path import join
+import argparse
+
+opt = argparse.ArgumentParser()
+opt.add_argument('targetDir', help='Каталог с кодом для пропарсинга.')
+
+args = opt.parse_args()
+
+root = '.'
+
+if args.targetDir:
+	root = args.targetDir
+
+fnames = []
+for path, subdirs, files in os.walk(root):
+	for name in files:
+		if name[-3:] == '.dm':
+			fnames.append(os.path.join(path, name))
+
+for fname in fnames:
+	with open(fname, 'ab+') as file:
+		file.seek(0)
+		lines = file.readlines()
+		if not lines:
+			continue
+		last = lines[-1]
+		if last[-1] != 10:
+			print('%s have no trailing newline!' % fname)
+			file.write(b'\n')
