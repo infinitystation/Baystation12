2945088681dda403e510f4d1c1b29b433d3a612e
Archemagus <32466328+Archemagus@users.noreply.github.com>
2021-03-13 23:43:13 +0300

    Update check-paths.sh

--- test/check-paths.sh
+++ test/check-paths.sh
@@ -36,7 +36,7 @@ exactly 1 "world<< uses" 'world<<|world[[:space:]]<<'
 exactly 2 "world.log<< uses" 'world.log<<|world.log[[:space:]]<<'
 exactly 214 "<< uses" '(?<!<)<<(?!<)' -P
 exactly 0 "incorrect indentations" '^( {4,})' -P
-exactly 35 "text2path uses" 'text2path'
+exactly 36 "text2path uses" 'text2path'
 exactly 3 "update_icon() override" '/update_icon\((.*)\)'  -P
 exactly 1 "goto uses" 'goto '
 exactly 536 "spawn uses" 'spawn\s*\(\s*(-\s*)?\d*\s*\)' -P
