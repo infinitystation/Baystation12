dae750a1b00bb7aa4bf6e824aab9ffe7874209af
Carl Ivanov <bdgtw502@mail.ru>
2021-05-23 15:17:26 +0900

    md5 check

--- test/run-test.sh
+++ test/run-test.sh
@@ -207,6 +207,7 @@ function run_code_tests {
     run_test_fail "no invalid spans" "grep -En \"<\s*span\s+class\s*=\s*('[^'>]+|[^'>]+')\s*>\" **/*.dm"
     run_test "code quality checks" "test/check-paths.sh"
     run_test "indentation check" "awk -f tools/indentation.awk **/*.dm"
+    run_test "check changelog example unchanged" "md5 'html/changelogs_infinity/example.yml'"
     run_test "check changelog example unchanged" "md5sum -c - <<< '4d41a720be649350d74c1fc375854e2f *html/changelogs_infinity/example.yml'"
     run_test "check tags" "python3 tools/TagMatcher/tag-matcher.py ."
     run_test "check color hex" "python3 tools/ColorHexChecker/color-hex-checker.py ."
